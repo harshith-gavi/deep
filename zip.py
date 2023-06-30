5be81acd7b2b29dfa0a55ff96421f27098b9b63b
harshith-gavi <91324439+harshith-gavi@users.noreply.github.com>
2023-06-30 16:16:20 +0200

    Update zip.py

--- zip.py
+++ zip.py
@@ -234,7 +234,7 @@ for _ in range(1, 5):
             model_spk.append(model.spk_out)
             del xx
         progress_bar.update(1)
-        torch.cuda.empty_cache(device = device_1)
+        torch.device_1.empty_cache()
         print(torch.cuda.memory_cached(device=device_1))
         print(torch.cuda.memory_cached(device=device_2))
 
