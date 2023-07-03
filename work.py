b79f8a34043140c992b66c10adbb5cb92f2a076c
harshith-gavi <91324439+harshith-gavi@users.noreply.github.com>
2023-07-03 17:07:17 +0200

    Update work.py

--- work.py
+++ work.py
@@ -193,13 +193,14 @@ class LSNN(nn.Module):
         L1 = L1.to(device_2)
         T_m = self.act(self.l1_T_m(L1 + self.u1))
         T_adp = self.act(self.l1_T_adp(L1 + self.b1))
-        self.u1, self.spk1, self.b1 = self.update_params(L1, self.u1, Spk, T_m, T_adp, self.b1)
+        self.u1, self.spk1, self.b1 = self.update_params(L1, self.u1, self.spk1, T_m, T_adp, self.b1)
+        
         self.spk1 = self.spk1.to(device_1)
         L2 = self.syn2(self.spk1)
         L2 = L2.to(device_2)
         T_m = self.act(self.l2_T_m(L2 + self.u2))
         T_adp = self.act(self.l2_T_adp(L2 + self.b2))
-        self.u2, self.spk2, self.b2  = self.update_params(L2, self.u2, Spk, T_m, T_adp, self.b2)
+        self.u2, self.spk2, self.b2  = self.update_params(L2, self.u2, self.spk2, T_m, T_adp, self.b2)
         
         self.spk2 = self.spk2.to(device_1)
         L3 = self.syn3(self.spk2)
