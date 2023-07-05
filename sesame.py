d7be6ac8caf1a88062546d39cfac4f1c9f96d6c2
harshith-gavi <91324439+harshith-gavi@users.noreply.github.com>
2023-07-05 12:31:43 +0200

    Update sesame.py

--- sesame.py
+++ sesame.py
@@ -85,9 +85,9 @@ class LSNN(nn.Module):
         self.u2 = torch.zeros(b_size, h_size[1]).to(device_2)
         self.u3 = torch.zeros(b_size, o_size).to(device_2)
 
-        self.b1 = torch.zeros(b_size, h_size[0]).to(device_1)
-        self.b2 = torch.zeros(b_size, h_size[1]).to(device_2)
-        self.b3 = torch.zeros(b_size, o_size).to(device_2)
+        b1 = torch.zeros(b_size, h_size[0]).to(device_1)
+        b2 = torch.zeros(b_size, h_size[1]).to(device_2)
+        b3 = torch.zeros(b_size, o_size).to(device_2)
 
         self.spk1 = torch.zeros(b_size, h_size[0]).to(device_1)       # Spikes
         self.spk2 = torch.zeros(b_size, h_size[1]).to(device_2)
@@ -129,7 +129,7 @@ class LSNN(nn.Module):
         nn.init.zeros_(self.o_T_adp.bias)
         nn.init.zeros_(self.o_T_m.bias)
 
-    def update_params(op, u_t, spk, t_m, t_adp, b_t):
+    def update_params(self, op, u_t, spk, t_m, t_adp, b_t):
         """
         Used to update the parameters
         INPUT: Layer output, Membrane Potential, Spikes, T_adp, T_m and Intermediate State Variable (b_t)
@@ -159,19 +159,19 @@ class LSNN(nn.Module):
         x_t = x_t.to(device_1)
         L1 = self.syn1(x_t)
         T_m = self.act(self.l1_T_m(L1 + self.u1))
-        T_adp = self.act(self.l1_T_adp(L1 + self.b1))
-        self.u1, self.spk1, self.b1 = self.update_params(L1, self.u1, self.spk1, T_m, T_adp, self.b1)
+        T_adp = self.act(self.l1_T_adp(L1 + b1))
+        self.u1, self.spk1, b1 = self.update_params(L1, self.u1, self.spk1, T_m, T_adp, b1)
         temp = self.spk1
         temp = temp.to(device_2)
         L2 = self.syn2(temp)
         T_m = self.act(self.l2_T_m(L2 + self.u2))
-        T_adp = self.act(self.l2_T_adp(L2 + self.b2))
-        self.u2, self.spk2, self.b2  = self.update_params(L2, self.u2, self.spk2, T_m, T_adp, self.b2)
+        T_adp = self.act(self.l2_T_adp(L2 + b2))
+        self.u2, self.spk2, b2  = self.update_params(L2, self.u2, self.spk2, T_m, T_adp, b2)
 
         L3 = self.syn3(self.spk2)
         T_m = self.act(self.o_T_m(L3 + self.u3))
-        T_adp = self.act(self.o_T_adp(L3 + self.b3))
-        self.u3, self.spk_out, self.b3 =  self.update_params(L3, self.u3, self.spk_out, T_m, T_adp, self.b3)
+        T_adp = self.act(self.o_T_adp(L3 + b3))
+        self.u3, self.spk_out, b3 =  self.update_params(L3, self.u3, self.spk_out, T_m, T_adp, b3)
         
         del x_t, T_m, T_adp, L1, L2, L3, temp
 
