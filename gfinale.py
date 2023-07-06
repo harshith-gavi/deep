c69e8b11ed8803bf477d4bbf7e9081159dff6312
harshith-gavi <91324439+harshith-gavi@users.noreply.github.com>
2023-07-06 17:05:10 +0200

    Update gfinale.py

--- gfinale.py
+++ gfinale.py
@@ -168,8 +168,7 @@ def es_geht():
                 xx = inputs.to_dense()[:, i, :]
                 b_spk = model(xx)
             
-            b_spk = b_spk.to(device_2)
-            model_spk.append(b_spk)
+            model_spk.append(b_spk.to(device_2)
             
             progress_bar.update(1)   
         progress_bar.close()
